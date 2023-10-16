e30748fe0b17881a74712f9d70e2fd405319f4ff
charles <charles_spfc@hotmail.com>
2023-10-15 21:33:48 -0300

    Add file

---
+++ src/sql/create-comments.sql
@@ -0,0 +1,11 @@
+CREATE TABLE comments (
+id INT,
+message TEXT NOT NULL, 
+created_at TIMESTAMP,
+post_id INT NOT NULL
+)
+
+ALTER TABLE comments ADD PRIMARY KEY (id);
+ALTER TABLE comments MODIFY id INT AUTO_INCREMENT;
+ALTER TABLE comments MODIFY created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE comments ADD CONSTRAINT FK_comments_post FOREIGN KEY (post_id) REFERENCES posts (id);
