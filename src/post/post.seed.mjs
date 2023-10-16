57a5a8596338e74a15551148d3f16908934cfd83
charlesanderson25 <charles_spfc@hotmail.com>
2023-10-16 15:42:56 -0300

    Add commentSeed and generateComment

--- src/post/post.seed.mjs
+++ src/post/post.seed.mjs
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker";
 
 import {
   createPost,
+  createPostComment,
   deletePost,
   listPosts,
   readPost,
@@ -9,6 +10,8 @@ import {
 } from "./post.model.service.mjs";
 
 const defaultLimit = 100;
+const minCommentCount = 3;
+const commentRange = 12;
 
 async function postSeed() {
   const limit = Number(process.argv[2] ?? defaultLimit);
@@ -18,10 +21,21 @@ async function postSeed() {
     const postData = generatePost();
 
     const post = await createPost(postData);
-    console.log(`Post criado com id: ${post.id}`);
+
+    await commentSeed(post);
   }
 
   console.log("Seed realizado com sucesso!");
+}
+
+async function commentSeed(post) {
+  const commentCount =
+    minCommentCount + Math.round(Math.random() * commentRange);
+  for (let index = 0; index < commentCount; index++) {
+    const comment = generateComment();
+    const addedComment = await createPostComment(post.id, comment);
+    console.log(`Post criado com id: ${addedComment.id}`);
+  }
 }
 
 function generatePost() {
@@ -30,6 +44,12 @@ function generatePost() {
     content: faker.lorem.words(5 + Math.round(Math.random() * 6)),
     // content: faker.lorem.paragraph(3 + Math.round(Math.random() * 7)),
     // created_at: faker.date.past({ years: 5 }).toJSON,
+  };
+}
+
+function generateComment() {
+  return {
+    message: faker.lorem.words(2 + Math.round(Math.random() * 5)),
   };
 }
 
