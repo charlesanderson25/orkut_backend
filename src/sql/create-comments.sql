7604f49e8ac1d1714a6e669e68c52906744497c2
charles <charles_spfc@hotmail.com>
2023-10-15 22:18:03 -0300

    Add insert into

--- src/sql/create-comments.sql
+++ src/sql/create-comments.sql
@@ -9,3 +9,5 @@ ALTER TABLE comments ADD PRIMARY KEY (id);
 ALTER TABLE comments MODIFY id INT AUTO_INCREMENT;
 ALTER TABLE comments MODIFY created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE comments ADD CONSTRAINT FK_comments_post FOREIGN KEY (post_id) REFERENCES posts (id);
+
+INSERT INTO comments (message, post_id) VALUES ('Teste 1', 110);
