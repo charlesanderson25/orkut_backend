f31afb4a86343cd1c36de5fae78ed17d480b5f4f
charlesanderson25 <charles_spfc@hotmail.com>
2023-10-19 13:51:41 -0300

    Changes createUser

--- src/user/user.model.service.mjs
+++ src/user/user.model.service.mjs
@@ -1,8 +1,45 @@
 import { connectionDataBase } from "../db.mjs";
 
+// export async function createUser(data) {
+//   try {
+//     const query = /*SQL*/ `INSERT INTO users(first_name, last_name, avatar, pass_word) VALUES(?, ?, ?, ?)`;
+//     const values = [
+//       data.first_name,
+//       data.last_name,
+//       data.avatar,
+//       data.pass_word,
+//     ];
+
+//     await connectionDataBase.promise().query(query, values);
+
+//     const [lastInsertResult] = await connectionDataBase
+//       .promise()
+//       .query("SELECT LAST_INSERT_ID() as lastInsertId");
+
+//     const lastInsertId = lastInsertResult[0].lastInsertId;
+
+//     const [result] = await connectionDataBase
+//       .promise()
+//       .query("SELECT * FROM users WHERE id = ?", [lastInsertId]);
+
+//     if (result.length === 1) {
+//       return result[0];
+//     } else {
+//       return "Nenhum registro inserido, por favor verifique!";
+//     }
+//   } catch (error) {
+//     console.error("Erro na consulta", error);
+//     throw error;
+//   }
+// }
+
 export async function createUser(data) {
   try {
-    const query = /*SQL*/ `INSERT INTO users(first_name, last_name, avatar, pass_word) VALUES(?, ?, ?, ?)`;
+    const query = `
+      INSERT INTO users (first_name, last_name, avatar, pass_word)
+      VALUES (?, ?, ?, ?);
+    `;
+
     const values = [
       data.first_name,
       data.last_name,
@@ -53,11 +90,12 @@ export async function readUser(id) {
   }
 }
 
-export async function readAllUsers() {
+export async function listAllUsers() {
   try {
     const queryCode = /*SQL*/ `SELECT * FROM users`;
     const [rows] = await connectionDataBase.promise().query(queryCode);
 
+    console.log(rows);
     return rows;
   } catch (error) {
     console.error("Erro na consulta", error);
